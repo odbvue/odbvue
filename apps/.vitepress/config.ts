d676695fe8325b2fb04850bc4f659ccc2e562881
Jānis Erlihs <erlihs@gmail.com>
2025-10-19 09:30:32 +0300

    wiki initial commit

---
+++ apps/.vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  srcDir: '..\\wiki',
+
+  title: 'OdbVue',
+  description: 'Take Ownership of Your Future',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['vue/server-renderer', 'vue'],
+      },
+    },
+  },
+})
