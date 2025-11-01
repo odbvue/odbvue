00cee42ff04a3a9d7cb090101c49dfbc9cb063d6
Jānis Erlihs <erlihs@gmail.com>
2025-11-01 10:09:40 +0200

    db fresh start

--- db/dist/releases/next/changes/featdb-ebr/_custom/.srcsciptsdummy.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset  SqlCl:1761958470308 stripComments:false logicalFilePath:featdb-ebr\_custom\.srcsciptsdummy.sql
--- sqlcl_snapshot dist\releases\next\changes\featdb-ebr\_custom\.srcsciptsdummy.sql:null:null:custom
-
-
-
--- db/dist/releases/next/changes/featdb-initial-setup/_custom/.srcapp001_create_schema_odbvue.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset  SqlCl:1761916068058 stripComments:false logicalFilePath:featdb-initial-setup\_custom\.srcapp001_create_schema_odbvue.sql
--- sqlcl_snapshot dist\releases\next\changes\featdb-initial-setup\_custom\.srcapp001_create_schema_odbvue.sql:null:null:custom
-
-
-
