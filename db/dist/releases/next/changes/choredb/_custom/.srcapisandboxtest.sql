35813f0a270dcfb293b26de0bcc0157a846c5180
Jānis Erlihs <erlihs@gmail.com>
2025-11-03 11:06:47 +0200

    feat(db): test

---
+++ db/dist/releases/next/changes/choredb/_custom/.srcapisandboxtest.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1762156465070 stripComments:false logicalFilePath:choredb\_custom\.srcapisandboxtest.sql
+-- sqlcl_snapshot dist\releases\next\changes\choredb\_custom\.srcapisandboxtest.sql:null:null:custom
+
+
+
