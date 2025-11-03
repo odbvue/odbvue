5a9742cff941195e1a65e124c2936c09560eef8f
Jānis Erlihs <erlihs@gmail.com>
2025-11-03 09:40:11 +0200

    db export: commit message

---
+++ db/src/database/odbvue/procedures/foo.sql
@@ -0,0 +1,8 @@
+create or replace procedure odbvue.foo as
+begin
+    dbms_output.put_line('foo');
+end;
+/
+
+
+-- sqlcl_snapshot {"hash":"d3113b9627728b2a6c01b108d9f68f6bb565346d","type":"PROCEDURE","name":"FOO","schemaName":"ODBVUE","sxml":""}
