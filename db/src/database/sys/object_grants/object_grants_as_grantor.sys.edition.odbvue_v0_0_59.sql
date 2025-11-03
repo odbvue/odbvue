8094f485b7f4423604af189e5c41aaef0f7fc43f
Jānis Erlihs <erlihs@gmail.com>
2025-11-03 08:07:22 +0200

    chore: normalize EOL to LF

--- db/src/database/sys/object_grants/object_grants_as_grantor.sys.edition.odbvue_v0_0_59.sql
+++
@@ -1,6 +0,0 @@
-grant use on sys.odbvue_v0_0_59 to odbvue;
-
-
-
-
--- sqlcl_snapshot {"hash":"2470c7def038e9ae0deac21d7b9a31e074a743f4","type":"OBJECT_GRANT","name":"object_grants_as_grantor.SYS.EDITION.ODBVUE_V0_0_59","schemaName":"SYS","sxml":""}
--- i13e/oci/basic/terraform/oci_config.tf
+++
@@ -1,1 +0,0 @@
-# This file is no longer used - tenancy_ocid is now configured in variables.tf
