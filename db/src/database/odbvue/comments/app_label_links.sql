4e1577e72a55e85df0816acfc04e55db2fd87c1c
Jānis Erlihs <erlihs@gmail.com>
2025-11-11 12:45:16 +0200

    feat(db): labels

---
+++ db/src/database/odbvue/comments/app_label_links.sql
@@ -0,0 +1,17 @@
+COMMENT ON TABLE odbvue.app_label_links IS
+    'Table for linking app_labels to entities';
+
+COMMENT ON COLUMN odbvue.app_label_links.entity_id_nm IS
+    'Numeric Id of the entity';
+
+COMMENT ON COLUMN odbvue.app_label_links.entity_id_vc IS
+    'Variable character Id of the entity';
+
+COMMENT ON COLUMN odbvue.app_label_links.entity_name IS
+    'Name of the entity';
+
+COMMENT ON COLUMN odbvue.app_label_links.label_id IS
+    'Id of the label';
+
+
+-- sqlcl_snapshot {"hash":"57142867528b515cebe4c94e3dc20e514bf14f62","type":"COMMENT","name":"app_label_links","schemaName":"odbvue","sxml":""}
