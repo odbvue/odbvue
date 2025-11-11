820e8435974b94eee524e9b27c066b8618eb914a
Jānis Erlihs <erlihs@gmail.com>
2025-11-11 11:47:02 +0200

    changeset: redeploy

---
+++ db/dist/releases/next/changes/featdb/odbvue/tables/dbtools$mcp_log.sql
@@ -0,0 +1,8 @@
+-- liquibase formatted sql
+-- changeset ODBVUE:1762854408481 stripComments:false  logicalFilePath:featdb\odbvue\tables\dbtools$mcp_log.sql
+-- sqlcl_snapshot db/src/database/odbvue/tables/dbtools$mcp_log.sql:273f4f26f6bcd72a36ec50d961ad4dc175d8ece0:null:drop
+
+/*  Uncomment drop statement after ensuring it is performing the correct actions
+DROP TABLE "ODBVUE"."DBTOOLS$MCP_LOG" CASCADE CONSTRAINTS;
+*/
+
