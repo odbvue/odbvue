654ecea0f0fd2fb28d2601d46e89b03918900d0c
Jānis Erlihs <erlihs@gmail.com>
2025-11-11 12:42:29 +0200

    chore(db): staged - purge release

--- db/src/database/odbvue/ref_constraints/cfk_app_label_links_label_id.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE odbvue.app_label_links
-    ADD CONSTRAINT cfk_app_label_links_label_id
-        FOREIGN KEY ( label_id )
-            REFERENCES odbvue.app_labels ( id )
-                ON DELETE CASCADE
-        ENABLE;
-
-
--- sqlcl_snapshot {"hash":"5d698e7231cafb9613c710296ad99ed2a27f313a","type":"REF_CONSTRAINT","name":"CFK_APP_LABEL_LINKS_LABEL_ID","schemaName":"ODBVUE","sxml":""}
