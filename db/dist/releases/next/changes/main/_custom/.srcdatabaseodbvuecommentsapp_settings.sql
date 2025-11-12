c190afa4ffbd95242c7e0754180e9d0684330261
Jānis Erlihs <erlihs@gmail.com>
2025-11-12 23:09:53 +0200

    feat(db): settings

---
+++ db/dist/releases/next/changes/main/_custom/.srcdatabaseodbvuecommentsapp_settings.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1762981598786 stripComments:false logicalFilePath:main\_custom\.srcdatabaseodbvuecommentsapp_settings.sql
+-- sqlcl_snapshot dist\releases\next\changes\main\_custom\.srcdatabaseodbvuecommentsapp_settings.sql:null:null:custom
+
+
+
---
+++ db/dist/releases/next/changes/main/_custom/.srcdatabaseodbvueindexesapp_settings_name.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1762981646928 stripComments:false logicalFilePath:main\_custom\.srcdatabaseodbvueindexesapp_settings_name.sql
+-- sqlcl_snapshot dist\releases\next\changes\main\_custom\.srcdatabaseodbvueindexesapp_settings_name.sql:null:null:custom
+
+
+
