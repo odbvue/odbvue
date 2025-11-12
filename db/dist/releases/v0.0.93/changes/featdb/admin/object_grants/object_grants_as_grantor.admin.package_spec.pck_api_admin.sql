8c1b0f1366dc2ada991c0625913e31e931b345a1
Jānis Erlihs <erlihs@gmail.com>
2025-11-12 22:23:49 +0200

    bugfix(db):  remove admin package from releases

--- db/dist/releases/v0.0.93/changes/featdb/admin/object_grants/object_grants_as_grantor.admin.package_spec.pck_api_admin.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset ADMIN:1762339248767 stripComments:false  logicalFilePath:featdb\admin\object_grants\object_grants_as_grantor.admin.package_spec.pck_api_admin.sql
--- sqlcl_snapshot db/src/database/admin/object_grants/object_grants_as_grantor.admin.package_spec.pck_api_admin.sql:null:00539b55e95a031aa29acc35e37f28b297688124:create
-
-GRANT EXECUTE ON admin.pck_api_admin TO odbvue;
-
