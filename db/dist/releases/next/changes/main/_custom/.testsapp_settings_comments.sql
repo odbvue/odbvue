3d8762ff5c298cfc5f55330f2acc5748ba67713d
Jānis Erlihs <erlihs@gmail.com>
2025-11-12 23:38:30 +0200

    feat(db): settings

---
+++ db/dist/releases/next/changes/main/_custom/.testsapp_settings_comments.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1762983430808 stripComments:false logicalFilePath:main\_custom\.testsapp_settings_comments.sql
+-- sqlcl_snapshot dist\releases\next\changes\main\_custom\.testsapp_settings_comments.sql:null:null:custom
+
+
+
---
+++ db/dist/releases/next/changes/main/_custom/.testspck_api_settings_spec.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1762983455115 stripComments:false logicalFilePath:main\_custom\.testspck_api_settings_spec.sql
+-- sqlcl_snapshot dist\releases\next\changes\main\_custom\.testspck_api_settings_spec.sql:null:null:custom
+
+
+
