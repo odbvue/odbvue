9086042601973bcaf99d527b9c5f348dc5f7414d
Jānis Erlihs <erlihs@gmail.com>
2025-11-13 15:07:41 +0200

    chore(db): not needed grants

---
+++ db/src/database/sys/object_grants/object_grants_as_grantor.sys.edition.odbvue_v_0_0_61.sql
@@ -0,0 +1,6 @@
+GRANT USE ON sys.odbvue_v_0_0_61 TO odbvue;
+
+
+
+
+-- sqlcl_snapshot {"hash":"f832315ac9d58323a1ff9fbfb07d92e2c986d38c","type":"OBJECT_GRANT","name":"object_grants_as_grantor.SYS.EDITION.ODBVUE_V_0_0_61","schemaName":"SYS","sxml":""}
