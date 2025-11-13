6581ea6ec944e7bd8c2bbc678a449a7bbed42ee7
Jānis Erlihs <erlihs@gmail.com>
2025-11-13 09:25:05 +0200

    feat(db): storage fix grants

--- db/dist/releases/v0.0.141/changes/featdb/c##cloud$service/object_grants/object_grants_as_grantor.c##cloud$service.package_spec.dbms_cloud$base.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset C##CLOUD$SERVICE:1763018046920 stripComments:false  logicalFilePath:featdb\c##cloud$service\object_grants\object_grants_as_grantor.c##cloud$service.package_spec.dbms_cloud$base.sql
--- sqlcl_snapshot db/src/database/c##cloud$service/object_grants/object_grants_as_grantor.c##cloud$service.package_spec.dbms_cloud$base.sql:null:d9793511abf1b17908e0edae6f9c2cc405fa64b0:create
-
-GRANT EXECUTE ON c##cloud$service.dbms_cloud$base TO odbvue;
-
--- db/src/database/c##cloud$service/object_grants/object_grants_as_grantor.c##cloud$service.package_spec.dbms_cloud$base.sql
+++
@@ -1,6 +0,0 @@
-GRANT EXECUTE ON c##cloud$service.dbms_cloud$base TO odbvue;
-
-
-
-
--- sqlcl_snapshot {"hash":"d9793511abf1b17908e0edae6f9c2cc405fa64b0","type":"OBJECT_GRANT","name":"object_grants_as_grantor.C##CLOUD$SERVICE.PACKAGE_SPEC.DBMS_CLOUD$BASE","schemaName":"C##CLOUD$SERVICE","sxml":""}
