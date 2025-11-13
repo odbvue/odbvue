334548459071b02d2fa674b73cf1c63081e6a03a
Jānis Erlihs <erlihs@gmail.com>
2025-11-13 15:42:56 +0200

    feat(db): admin package

--- db/dist/releases/v0.0.146/changes/main/_custom/admin_pck_api_syno.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset  SqlCl:1763040455696 stripComments:false logicalFilePath:main\_custom\admin_pck_api_syno.sql
--- sqlcl_snapshot dist\releases\next\changes\main\_custom\admin_pck_api_syno.sql:null:null:custom
-
-
-CREATE OR REPLACE SYNONYM odbvue.pck_api_admin FOR admin.pck_api_admin;
-
-
-
