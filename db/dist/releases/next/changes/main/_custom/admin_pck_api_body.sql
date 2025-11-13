1b6561f7a86121b9e1c8a09b49f3a686f326b0dd
Jānis Erlihs <erlihs@gmail.com>
2025-11-13 15:29:36 +0200

    feat(db): admin package

---
+++ db/dist/releases/next/changes/main/_custom/admin_pck_api_body.sql
@@ -0,0 +1,90 @@
+-- liquibase formatted sql
+-- changeset  SqlCl:1763040445751 stripComments:false logicalFilePath:main\_custom\admin_pck_api_body.sql
+-- sqlcl_snapshot dist\releases\next\changes\main\_custom\admin_pck_api_body.sql:null:null:custom
+
+
+
+create or replace package BODY admin.pck_api_admin AS 
+
+procedure acl_append_host( 
+    p_schema VARCHAR2,
+    p_host VARCHAR2, 
+    p_lower_port PLS_INTEGER, 
+    p_upper_port PLS_INTEGER, 
+    p_privilege VARCHAR2 
+) AS 
+BEGIN
+    DBMS_NETWORK_ACL_ADMIN.APPEND_HOST_ACE (
+            host => p_host,
+            lower_port => p_lower_port,
+            upper_port => p_upper_port,
+            ace => xs$ace_type(
+                privilege_list => xs$name_list(p_privilege),
+                principal_name => UPPER(p_schema),
+                principal_type => xs_acl.ptype_db
+            )
+        );
+
+        COMMIT;
+
+    END;
+
+    procedure acl_remove_all_hosts( 
+        p_schema VARCHAR2
+    ) AS 
+    BEGIN
+        FOR e IN (
+            SELECT
+                host,
+                lower_port,
+                upper_port,
+                privilege
+            FROM dba_host_aces
+            WHERE principal = UPPER(p_schema)
+        ) LOOP
+            BEGIN
+                DBMS_NETWORK_ACL_ADMIN.REMOVE_HOST_ACE (
+                    e.host,
+                    e.lower_port,
+                    e.upper_port,
+                    xs$ace_type(
+                        privilege_list => xs$name_list(e.privilege),
+                        principal_name => UPPER(p_schema),
+                        principal_type => xs_acl.ptype_db
+                    )
+                );
+                COMMIT;
+            EXCEPTION
+                WHEN OTHERS THEN
+                    DBMS_OUTPUT.PUT_LINE('Error removing ACE for host ' || e.host || 
+                        ' -> ' || e.privilege || 
+                        ' (' || e.lower_port || '-' || e.upper_port || '): ' || SQLERRM);
+            END;        
+        END LOOP;
+    END;
+
+
+procedure create_cred( 
+    p_schema VARCHAR2,
+    p_cred_name VARCHAR2, 
+    p_username VARCHAR2, 
+    p_password VARCHAR2 
+) AS
+BEGIN
+
+    IF UPPER(p_schema) != UPPER(USER) THEN
+        RAISE_APPLICATION_ERROR(-20001, 'You can only create credentials in your own schema');
+    END IF;
+
+    DBMS_CLOUD.CREATE_CREDENTIAL(
+        credential_name => p_cred_name,
+        username        => p_username,
+        password        => p_password
+    );
+
+    COMMIT;
+
+END;
+
+end pck_api_admin;
+
