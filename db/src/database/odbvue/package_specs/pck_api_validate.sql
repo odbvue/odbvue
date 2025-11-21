8f492b2192c0e6ce0ffeeb84baddf46e9771f1c9
Jānis Erlihs <erlihs@gmail.com>
2025-11-22 01:04:58 +0200

    feat(db): bugfix(db): validation api fails on deploy

--- db/src/database/odbvue/package_specs/pck_api_validate.sql
+++ db/src/database/odbvue/package_specs/pck_api_validate.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE PACKAGE odbvue.pck_api_validate AS -- Package provides methods
 
     FUNCTION validate ( -- Function validates the provided value against the specified rules in JSON format
         p_value IN VARCHAR2, -- Value to validate
-        p_rules IN CLOB -- Validation rules in JSON format
+        p_rules IN CLOB -- Validation rules in JSON format [{type, params, message}]
     ) RETURN VARCHAR2; -- Returns NULL if valid; otherwise error message
 END pck_api_validate;
 /
 
 
--- sqlcl_snapshot {"hash":"1940c6d16093d134b0ddbcc8988facae8a147dd3","type":"PACKAGE_SPEC","name":"PCK_API_VALIDATE","schemaName":"ODBVUE","sxml":""}
+-- sqlcl_snapshot {"hash":"895fb912587a926520a4ba092a4375e7071c42cb","type":"PACKAGE_SPEC","name":"PCK_API_VALIDATE","schemaName":"ODBVUE","sxml":""}
