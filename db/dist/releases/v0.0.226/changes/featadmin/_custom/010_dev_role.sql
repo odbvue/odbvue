a77657e073397390f4d1f72c02294ff695355285
Jānis Erlihs <erlihs@gmail.com>
2025-12-03 12:15:37 +0200

    feat(apps): role contraint violation

--- db/dist/releases/v0.0.226/changes/featadmin/_custom/010_dev_role.sql
+++ db/dist/releases/v0.0.226/changes/featadmin/_custom/010_dev_role.sql
@@ -3,14 +3,12 @@
 -- sqlcl_snapshot dist\releases\next\changes\featadmin\_custom\010_dev_role.sql:null:null:custom
 
 
-MERGE INTO app_roles dest
-USING (SELECT 'DEVELOPER' AS role,
-              'Developers Role' AS description
-       FROM dual) src
-ON (dest.role = src.role)
-WHEN MATCHED THEN
-  UPDATE SET dest.description = src.description
-WHEN NOT MATCHED THEN
-  INSERT (role, description)
-  VALUES (src.role, src.description);
+INSERT INTO app_roles (role, description)
+SELECT 'DEVELOPER' AS role,
+       'Developers Role' AS description 
+FROM dual
+WHERE NOT EXISTS (SELECT 1 
+                  FROM app_roles 
+                  WHERE role = 'DEVELOPER');
+
 
